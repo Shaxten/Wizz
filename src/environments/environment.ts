63ccec75988fad5cd8491a91b4307446a674bec5
Shaxten <joeycadoo@hotmail.com>
2026-04-16 11:14:38 -0400

    Add GitHub Pages deployment workflow

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://iergbqoxagoqpvcgulrr.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcmdicW94YWdvcXB2Y2d1bHJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMjk1MTUsImV4cCI6MjA5MTkwNTUxNX0.MIKHrDQiHTDrDP2Zihge7bknbQhdgeUQczE_tbijhj0',
+};
